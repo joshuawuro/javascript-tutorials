2dfdfaec057cee60b07c3180f30f09f429439115
Joshua Awuro <joshuawuro@gmail.com>
2024-05-29 22:44:48 -0700

    secrete message with the use of js arrays methods

---
+++ mini-projects/secret-message.js
@@ -0,0 +1,16 @@
+//SECRET MESSAGE
+
+/**
+ * Using array methods, you will transform an array of strings into a secret message
+ */
+
+let secretMessage =['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out', '-2015', 'Chris', 'Pine.', 'Learn', 'JavaScript']
+
+secretMessage.pop();
+console.log(secretMessage.length);
+secretMessage.push('to', 'program');
+sessionStorage[7] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(6, 4, 'know');
+console.log(secretMessage.join(' '));
